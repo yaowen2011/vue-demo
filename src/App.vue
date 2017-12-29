e542b28432430a24fefde5155a16d4f690432206
xiaoming <xiaoming@xm.com>
2017-12-29 16:50:34 +0800

    03-首页头部和底部导航的处理

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="cms_container">
     <!-- App.vue 是项目的根组件，所有的内容，都展示在App.vue中 -->
-    这是 黑马vue项目
+    <mt-header title="黑马程序员 | SH16">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
 
     <!-- 路由出口 -->
     <router-view></router-view>
+
+    <!-- 页脚 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ import Vue from 'vue'
 // 2 导入 App.vue 组件
 import App from './App.vue'
 
+// 导入 mui的样式
+import './lib/mui/css/mui.css'
+// 导入自定义样式
+import './assets/css/app.css'
+
 // 导入 路由配置
 // ./router 相当查找 ./router/index.js ，index.js 可以省略
 import router from './router'
+
+// 导入 mint-ui 组件
+import { Header, Button } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Button.name, Button)
 
 // 3 渲染组件
 const vm = new Vue({
