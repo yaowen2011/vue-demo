adfa38cf14fe3e9b5bd14b6af9c435eaea14eced
xiaoming <xiaoming@xm.com>
2017-12-31 10:11:14 +0800

    06-首页九宫格菜单完成

--- README.md
+++ README.md
@@ -1,4 +1,91 @@
-# 项目的说明
+# Vue 项目
+
+## 技术
+
+```html
+vue + axios + vue-router + vuex
+UI库和组件库：MintUI + MUI
+webpack + ES6
+```
+
+## 项目依赖
+
+```html
+---- 开发依赖 ----
+npm i -D webpack webpack-dev-server html-webpack-plugin style-loader css-loader sass-loader node-sass url-loader file-loader babel-core babel-loader babel-preset-env vue-loader vue-template-compiler
+
+组件按需加载插件
+npm install babel-plugin-component -D
+
+---- 项目依赖 ----
+npm i -S vue vue-router axios mint-ui
+npm i -S moment vue-preview vuex
+```
+
+## 项目功能
+
+- 1 App组件（项目入口）搭建
+  - header（mint-ui header组件）
+  - footer（注意MUI样式覆盖的问题 - tabbar）
+  - content（路由的使用，别忘了use注册路由）
+- 2 首页
+  - 轮播图（mint-ui Swipe）
+  - 九宫格（mui grid-default 60px）
+- 3 新闻咨询
+  - 新闻列表（mui 中的 media-list）
+  - 新闻详情
+  - 评论
+- 4 图片分享
+  - 图片列表（mui tab-top-webview-main） http://dev.dcloud.net.cn/mui/ui/#scroll
+  - 图片详情（mint-ui lazy-load）
+  - 图片详情预览（npm i -S vue-preview）
+- 5 商品购买
+  - 商品列表
+  - 商品详情（mui card、numbox）
+- 6 购物车
+  - 每个商品（card）
+  - 开关（mint-ui switch）
+  - 购物车商品总数 和 总价
+  - 商品删除
+
+```js
+// 有利于开发期间定位错误信息
+devtool: 'eval-source-map',
+```
+
+```css
+/* 文字内容超过长度以后省略号显示 */
+.text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+```
+
+```css
+/*
+  photo/List.vue 组件中使用mui scroll插件后，在Chrome浏览器中有警告提示
+  参考这篇文章，查看原因：
+  scroll滚动警告的处理方式参考：http://www.jianshu.com/p/baf61adc8667
+
+  解决方案：
+*/
+
+.mui-scroll {
+  touch-action: none;
+}
+```
+
+```js
+// vue-router 编程式导航：
+// https://router.vuejs.org/zh-cn/essentials/navigation.html
+showInfo(id) {
+  this.$router.push('/home/photoinfo/' + id)
+
+  // name 是路由规则中的配置的name属性
+  this.$router.push({ name: 'user', params: { userId: 123 }})
+}
+```
 
 ## 项目中路由的处理
 
@@ -16,3 +103,16 @@
 ## 项目中经常遇到的问题
 
 - 1 样式覆盖，我们自己的样式应该在第三方库之后引入，才会覆盖第三方库的默认样式
+
+## vue单文件组件
+
+- 知道：`style` 和 `template` 都可以使用预编译器
+- [Vue 单文件组件 预编译器文档](https://vue-loader.vuejs.org/zh-cn/configurations/pre-processors.html)
+
+```html
+<!-- 使用 pug 模板引擎 -->
+<template lang="pug"></template>
+
+<!-- 使用 SASS 预编译器 -->
+<style lang="scss"></style>
+```
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <!-- 轮播图 -->
     <mt-swipe :auto="4000" :speed="500" :showIndicators="false">
       <mt-swipe-item v-for="(item, index) in list" :key="index">
         <img :src="item.img" :alt="item.url">
@@ -11,6 +12,28 @@
         <img src="http://ofv795nmp.bkt.clouddn.com/vuelogobanner2-1.jpg" alt="">
       </mt-swipe-item> -->
     </mt-swipe>
+
+    <!-- 九宫格菜单 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-1"></span>
+              <div class="mui-media-body">新闻咨询</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-2"></span>
+              <div class="mui-media-body">图片分享</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-3"></span>
+              <div class="mui-media-body">商品购买</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-4"></span>
+              <div class="mui-media-body">留言反馈</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-5"></span>
+              <div class="mui-media-body">视频专区</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-6"></span>
+              <div class="mui-media-body">联系我们</div></a></li>
+    </ul>
   </div>
 </template>
 
@@ -19,6 +42,7 @@ export default {
   // 提供数据
   data() {
     return {
+      // 轮播图数组
       list: []
     }
   },
@@ -29,25 +53,73 @@ export default {
     // this 就是 Vue 的实例对象
     // console.log(this.$http);
 
-    this.$http
-      .get('http://vue.studyit.io/api/getlunbo')
-      .then(res => {
-        // console.log(res);
-        const data = res.data
-        if (data.status === 0) {
-          this.list = data.message
-        }
-      })
+    this.getLunbo()
+  },
+
+  methods: {
+    // 用来获取轮播图数据
+    getLunbo() {
+      this.$http
+        .get('http://vue.studyit.io/api/getlunbo')
+        .then(res => {
+          // console.log(res);
+          const data = res.data
+          // 只有 status 为0，那么才表示数据请求成功
+          if (data.status === 0) {
+            this.list = data.message
+          }
+        })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  /* 
+    style的lang属性 用来指定使用哪个预编译的CSS
+    lang="scss" 表示使用 SASS 中的 scss 语法格式
+    lang="less" 表示使用 LESS 的语法
+
+    参考文档：
+    https://vue-loader.vuejs.org/zh-cn/configurations/pre-processors.html
+  */
   .mint-swipe {
     height: 180px;
   }
 
   .mint-swipe-item img {
     width: 100%;
+  }
+
+  .mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    border: 0;
+
+    .mui-table-view-cell {
+      border: 0;
+    }
+  }
+  .mui-icon {
+    width: 60px;
+    height: 60px;
+    background-size: cover;
+  }
+  .mui-icon-1 {
+    background-image: url(../../assets/image/menu1.png)
+  }
+  .mui-icon-2 {
+    background-image: url(../../assets/image/menu2.png)
+  }
+  .mui-icon-3 {
+    background-image: url(../../assets/image/menu3.png)
+  }
+  .mui-icon-4 {
+    background-image: url(../../assets/image/menu4.png)
+  }
+  .mui-icon-5 {
+    background-image: url(../../assets/image/menu5.png)
+  }
+  .mui-icon-6 {
+    background-image: url(../../assets/image/menu6.png)
   }
 </style>
